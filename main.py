26493f8185736565db0adcdcc686f2d51105e7a6
Parsa Samadnejad <troddenspade@gmail.com>
2021-12-28 22:19:50 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import gym
 import torch
+import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
+from pytorch.Federator import Federator
 from pytorch.Agent import Agent
 from pytorch.QNetwork import FCQ
 from pytorch.ReplayBuffer import ReplayBuffer
 
 if __name__ == "__main__":
+    N_AGENTS = 3
+
     net_args = {
         "hidden_layers":(32,64),
         "activation_fn":torch.nn.functional.relu,
         "optimizer":torch.optim.Adam,
-        "learning_rate":0.0005,
+        "learning_rate":0.001,
     }
 
     args = {
@@ -25,9 +30,18 @@ if __name__ == "__main__":
         "min_buffer": 64
     }
 
-    agent = Agent(**args)
-    rewards, evals = agent.train(300)
+    # fed = Federator(n_agents=5, update_rate=50, args=args)
+    # rewards = fed.train(400)
 
-    plt.plot(rewards)
-    plt.plot(evals)
+    ag = Agent(**args)
+    rewards_ = np.zeros(400)
+    for r in tqdm(range(400)):
+        ag.step(50)
+        rewards_[r] = ag.evaluate()
+
+    print(ag.episode_count)
+    # plt.plot(rewards, color="b")
+    plt.plot(rewards_, color="r")
+    # plt.legend()
     plt.show()
+    
