945adf22ea8f13b3645513367b78b79c3633b409
Parsa Samadnejad <troddenspade@gmail.com>
2021-12-31 20:24:20 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,43 +5,57 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 from pytorch.Federator import Federator
-from pytorch.Agent import Agent
+from pytorch.DQN import Agent
 from pytorch.QNetwork import FCQ
 from pytorch.ReplayBuffer import ReplayBuffer
 
 if __name__ == "__main__":
-    N_AGENTS = 3
-
-    net_args = {
-        "hidden_layers":(32,64),
-        "activation_fn":torch.nn.functional.relu,
-        "optimizer":torch.optim.Adam,
-        "learning_rate":0.001,
-    }
 
     args = {
         "env_fn": lambda : gym.make("CartPole-v1"),
         "Qnet": FCQ,
         "buffer": ReplayBuffer,
 
-        "epsilon": 0.1,
-        "gamma": 1,
+        "net_args" : {
+            "hidden_layers":(512, 128),
+            "activation_fn":torch.nn.functional.relu,
+            "optimizer":torch.optim.RMSprop,
+            "learning_rate":0.0005,
+        },
+
+        "max_epsilon": 1.0,
+        "min_epsilon": 0.1,
+        "decay_steps": 2000,
+        "gamma": 0.99,
         "target_update_rate": 15,
         "min_buffer": 64
     }
 
-    # fed = Federator(n_agents=5, update_rate=50, args=args)
-    # rewards = fed.train(400)
+    n_runs = 1000
+    n_agents = 3
+    n_iterations = 10
+    update_rate = 30
 
-    ag = Agent(**args)
-    rewards_ = np.zeros(400)
-    for r in tqdm(range(400)):
-        ag.step(50)
-        rewards_[r] = ag.evaluate()
+    fed_rewards = np.zeros(n_runs)
+    for i in range(n_iterations):
+        fed = Federator(n_agents=n_agents, update_rate=update_rate, args=args)
+        fed_rewards += fed.train(n_runs)
+    fed_rewards /= n_iterations
+    with open('fed_rewards.npy', 'wb') as f:
+        np.save(f, fed_rewards)
 
-    print(ag.episode_count)
-    # plt.plot(rewards, color="b")
-    plt.plot(rewards_, color="r")
-    # plt.legend()
+    single_rewards = np.zeros(n_runs)
+    for i in range(n_iterations):
+        ag = Agent(**args)
+        for r in tqdm(range(n_runs)):
+            ag.step(update_rate)
+            single_rewards[r] += ag.evaluate()
+    single_rewards /= n_iterations
+    with open('single_rewards.npy', 'wb') as f:
+        np.save(f, single_rewards)
+
+    plt.plot(fed_rewards, color="b", label="federated")
+    plt.plot(single_rewards, color="r", label="single")
+    plt.legend()
     plt.show()
     
